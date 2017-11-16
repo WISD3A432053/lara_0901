f0dc2d652bdef02e3e3537fcae14d9747fafb591
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-16 17:02:34 +0800

    設定 AdminPostsController對應的 function

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -7,14 +7,15 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 
 use App\Post;
+
 class AdminPostsController extends Controller
 {
 
     public function index()
     {
-        $posts=Post::orderBy('created_at','DESC')->get();
-        $data=['posts'=>$posts];
-        return view('admin.posts.index',$data);
+        $posts = Post::orderBy('created_at', 'DESC')->get();
+        $data = ['posts' => $posts];
+        return view('admin.posts.index', $data);
     }
 
     public function create()
@@ -28,4 +29,10 @@ class AdminPostsController extends Controller
 
         return view('admin.posts.edit', $data);
     }
+
+    public function store()
+    {
+        
+    }
+
 }
